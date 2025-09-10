14e0075fe137145698d35ec9d14f187b13cfb8b0
affrinnisha <safrinseyed24@gmail.com>
2025-09-10 13:55:26 +0400

    Update student result management.c

--- student result management.c
+++ student result management.c
@@ -4,23 +4,24 @@
 #include<stdio.h>
 #include<string.h>
 int main(){
-    int choice,total, roll_num,marks[3];
+    int choice,total=0, roll_num,marks[3];
     float avg;
     char grade;
-    char name;
+    char name[50];
     do {
         printf("======STUDENT RESULT MANAGEMENT SYSTEM======\n");
         printf("1.add student details\n");
          printf("2.display student details\n");
-
-            printf("3.exit\n ");
+        printf("3.exit\n ");
+        printf("enter your choice");
             scanf("%d",&choice);
 
 
             switch(choice){
                 case 1:
+                    total=0;
                 printf("enter student name: ");
-                scanf("%s",&name);
+                scanf("%s",name);
 
                 printf("enter student roll number: ");
                 scanf("%d",&roll_num);
@@ -30,13 +31,30 @@ int main(){
                     scanf("%d",&marks [i]);
                     total += marks[i];
                 }
-                avg= total/3;
+                avg= (float)total/3;
                 if (avg>=90) grade ='A';
                  else if (avg>=80) grade ='B';
                  else if (avg>=50) grade='C';
                 else grade ='D';
                 printf("details saved!\n");
                 break;
+                case 2:
+                printf("the name of the student is : %s\n",name);
+                printf("the roll number of the student is %d\n",roll_num);
+                    for( int i=0;i<3;i++){
+                printf("the mark of the student in subject %d:%d\n",i+1,marks[i]);
+                    }
+                printf("the total of the student is: %d\n",total);
+                printf("the avg of the student is :%f\n",avg);
+                printf("the grade of the student is:%c\n",grade);
+                break;
+                case 3:
+                printf("exiting the program....");
+                default:
+                printf("invalid choice.... try again\n");
+                break;
             }
-            } while ( choice !=5);
+        
+            } while ( choice !=3);
     }
+
